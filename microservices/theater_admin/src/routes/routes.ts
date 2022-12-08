d18ffbfeb035fbd6ae73e59450d72c4b8dcf4ae7
ilya_pindrus <ipindrus@umass.edu>
2022-12-07 21:41:41 -0500

    feat: added authentication for admin endpoint

--- microservices/theater_admin/src/routes/routes.ts
+++ microservices/theater_admin/src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import { getSales } from '../controllers/getSalesController';
+import { adminAuth } from '../auth/auth';
 
 const router = Router();
 
-router.get('/api/revenue', getSales);
+// check if user if theater admin before executing endpoint
+router.get('/api/revenue', adminAuth, getSales);
 
 export {router};
