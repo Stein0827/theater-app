7c55d362c429da3d97c8ccea5d5ee818c0b1b460
Alan Castillo <alancastillo@Alans-Air.hsd1.ma.comcast.net>
2022-12-09 16:24:06 -0500

    feat: implements db into payments microservice

--- microservices/payments/src/controllers/createPaymentController.ts
+++ microservices/payments/src/controllers/createPaymentController.ts
@@ -3,13 +3,12 @@ import {PaymentRequest } from '../types.js';
 import {PaymentModel} from '../models/paymentModel.js'
 
 export const createPayment = async (req: Request, res: Response) => {
-  const data: PaymentRequest = req.body;
-
   try {
+    const data: PaymentRequest = req.body;
     const paymentModel = new PaymentModel(data);
-    const payment = paymentModel.createPayment();
-    res.status(200).send(payment);
+    const newPayment = await paymentModel.createPayment().catch((err)=> {throw err});
+    res.status(200).send(newPayment);
   } catch (err) {
-    res.status(400).send("Error: error creating movie in database");
-  }    
+    res.status(400).send(err);
+  }       
 }
--- microservices/payments/src/types.ts
+++ microservices/payments/src/types.ts
@@ -1,5 +1,27 @@
+import { RowDataPacket } from "mysql2"
+
+export interface IPayment extends RowDataPacket {
+    payment_id?: number;
+    movie_id?: number;
+    theater_id?: string;
+    time?: string;
+    price?: string;
+    email?: string;
+    fname?: string;
+    lname?: string;
+    cardnum?: string;
+    seccode?: string;
+    cardexp?: string;
+    bstreet?: string;
+    bunit?: string;
+    bstate?: string;
+    bcountry?: string;
+    zip?: string;
+}
+
 export type PaymentRequest = {
-    movie_id?: string;
+    payment_id?: number;
+    movie_id?: number;
     theater_id?: string;
     time?: string;
     price?: string;
