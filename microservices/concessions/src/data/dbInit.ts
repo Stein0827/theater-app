abaaad9dedb587b9be92bf1306d514fb348730af
Stein0827 <shixiuquanpaul@gmail.com>
2022-12-12 18:45:46 -0500

    fix: updated image for dbinit

--- microservices/concessions/src/data/dbInit.ts
+++ microservices/concessions/src/data/dbInit.ts
@@ -24,11 +24,11 @@ export async function initDB() {
 
     const concessions = db.collection('concessions');
     const result = await concessions.insertMany([
-        { name: 'coke', type: 'soft drink', price: 2.99, image: "https://preview.redd.it/r7da8ziv4n711.jpg?auto=webp&s=3c751551cf2d8b228fb43474da596ae597caddff" },
-        { name: 'popcorn', type: 'snack', price: 6.99, image: "https://i0.wp.com/spainonafork.com/wp-content/uploads/2019/04/popcornHOR-11.png?fit=750%2C750&ssl=1" },
-        { name: "Lay's chip classic", type: 'snack', price: 3.99, image: "http://mobileimages.lowes.com/product/converted/028400/028400097802.jpg" },
-        { name: "Lay's chip onion", type: 'snack', price: 3.99, image: "https://target.scene7.com/is/image/Target/GUEST_41855ae0-77f0-482d-a634-e978cfe038ca?wid=488&hei=488&fmt=pjpeg" },
-        { name: "Lay's chip barbecue", type: 'snack', price: 3.99, image: "https://m.media-amazon.com/images/I/81fXU7rqRhL.jpg" },
+        { name: 'coke', type: 'soft drink', price: 2.99, image: "https://static.specsonline.com/wp-content/uploads/2020/10/004900000634.jpg" },
+        { name: 'popcorn', type: 'snack', price: 6.99, image: "https://img.freepik.com/premium-vector/popcorn-striped-tub_157999-54.jpg?w=2000" },
+        { name: "Lay's chip classic", type: 'snack', price: 3.99, image: "https://m.media-amazon.com/images/I/813axPlVxBL.jpg" },
+        { name: "Lay's chip onion", type: 'snack', price: 3.99, image: "https://images.albertsons-media.com/is/image/ABS/970009650-C1N1?$ng-ecom-pdp-mobile$&defaultImage=Not_Available" },
+        { name: "Lay's chip barbecue", type: 'snack', price: 3.99, image: "https://m.media-amazon.com/images/I/919O9NwammL.jpg" },
     ]);
 
     console.log(`Initialized ${result.insertedCount} products`);
