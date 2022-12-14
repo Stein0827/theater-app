8195a736ea5f52a592fbde4b28aa682f50eac6a0
Stein0827 <shixiuquanpaul@gmail.com>
2022-12-14 13:32:46 -0500

    feat: added new event in theater service

--- microservices/theaters/src/controllers/eventController.ts
+++ microservices/theaters/src/controllers/eventController.ts
@@ -1,15 +1,17 @@
 import express, {Express, Request, Response} from 'express';
 import { TheaterRequest } from '../types.js';
 import { TheaterModel } from '../models/TheaterModel.js'
+import { publishEvent } from '../events/publishEvent.js';
 
 export const respondToEvent = async (req: Request, res: Response) => {
   try {
     const event = req.body;
     const model = new TheaterModel(event.eventData);
-    let result = {};
+    let result;
 
     if (event.eventType === "userCreated") {
-      result = `Theater created: ${await model.createTheater()}`
+      result = await model.createTheater();
+      await publishEvent("theaterWithId", {"username": event.eventData.username, "theater_id": result.id.toString()});
     }
 
     if (result === undefined) {
