08c01efb387c2fe68d6438fa7ba9cc4740be202e
ilya_pindrus <ipindrus@umass.edu>
2022-12-14 22:14:55 -0500

    chore: merge with main

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PaymentCreate } from "./Payment/PaymentCreate";
 import { Route, Routes, Navigate } from "react-router-dom";
 import { Signin }  from "./user/Signin";
@@ -10,10 +10,6 @@ import { Admin } from "./Admin/Admin"
 export const App = () => {
   const [theaterId, setTheaterId] = useState("");
   const updateTheaterId = (theaterId: string) => {setTheaterId(theaterId)}
-  //const [theaterId, setTheaterId] = useState("00000001639189e929544c75");
-  const [movie, setMovie] = useState(tempMovieEx);
-  const [showing, setShowing] = useState("1:30PM");
-  const [zipcode, setZipcode] = useState({"zip": "01090"});
 
   return (
     <Routes>
