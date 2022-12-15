7fa746a8d4f023b99088e9b04305bd5ab720244a
ilya_pindrus <ipindrus@umass.edu>
2022-12-15 17:52:36 -0500

    before yarn install

--- client/src/Admin/AdminRevenue.tsx
+++ client/src/Admin/AdminRevenue.tsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import rd3 from 'react-d3';
+import { Line } from "react-chartjs-2";
 
 export const GenerateChart = ({ theaterId }: { theaterId: string }) => {
     const [data, setData] = useState({revObj: [], totalTicketRevenue: 0, totalConcessionRevenue: 0});
     
-    const LineChart = rd3.LineChart;
-
     const fetchTheaterRevenue = async () => {
         const revRes = await axios.post('http://localhost:4006/api/revenue', [
             theaterId
@@ -15,13 +13,19 @@ export const GenerateChart = ({ theaterId }: { theaterId: string }) => {
     }
 
    const modifyData = (data: any) => {
-      const res = [{"name": "Ticket Revenue", "values": [] as any}, {"name": "Concession Revenue", values: [] as any}];
-      for (let i = 0; i < data.length; i++) {
-        const entry = data[i];
-        res[0].values.push({"x": entry.date, "y": entry.ticketRevenue});
-        res[1].values.push({"x": entry.date, "y": entry.concessionRevenue});
+      return {
+        "labels": data.map((d: any) => d.date),
+        "datasets": [
+            {
+                "label": "Ticket Revenue",
+                "data": data.map((d: any) => d.ticketRevenue),
+            },
+            {
+                "label": "Concession Revenue",
+                "data": data.map((d: any) => d.concessionsRevenue), 
+            }
+        ]
       }
-      return res;
    }
 
     useEffect(() => {
@@ -29,14 +33,22 @@ export const GenerateChart = ({ theaterId }: { theaterId: string }) => {
     }, []);
 
     return (
-        <div>
-            <LineChart
-                legend={true}
-                data={modifyData(data.revObj)}
-                width={500}
-                height={300}
-                title="Line Chart"
-            />
-        </div>
+        <div className="chart-container">
+        <h2 style={{ textAlign: "center" }}>Line Chart</h2>
+        <Line
+          data={modifyData(data.revObj)}
+          options={{
+            plugins: {
+              title: {
+                display: true,
+                text: "Users Gained between 2016-2020"
+              },
+              legend: {
+                display: false
+              }
+            }
+          }}
+        />
+      </div>
     );
 };
