c2bd4662b59b0089c61b325e64e3c26b085a7842
ilya_pindrus <ipindrus@umass.edu>
2022-12-14 22:01:37 -0500

    fix: modified register function to interact with sign up page

--- microservices/theater_identity/src/events/subscribeToEventBus.ts
+++ microservices/theater_identity/src/events/subscribeToEventBus.ts
@@ -4,7 +4,7 @@ export async function subscribeToEventbus() {
     const res = await axios.post('http://eventbus:4000/api/v1/subscribe', {
         url: 'http://theater_identity:4007/api/v1/events',
         events: [
-            'theaterCreated',
+            'theaterWithId',
             'theaterDeleted'
         ]
     });
--- microservices/theater_identity/src/index.ts
+++ microservices/theater_identity/src/index.ts
@@ -9,6 +9,12 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 app.use(router);
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', ['http://localhost:3000']);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 // Initialize the database
 initDB();
