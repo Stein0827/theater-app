d933f23d338eb535609044bd165d55fa8def226d
ilya_pindrus <ipindrus@umass.edu>
2022-12-15 15:26:52 -0500

    fix: fix file name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { PaymentCreate } from "./Payment/PaymentCreate";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { Signin }  from "./User/Signin";
+import { Signin }  from "./User/SignIn";
 import { GetTheaterByZip } from './Theater/GetTheaterByZip';
 import { TheaterDetail } from './Theater/TheaterDetail';
 import { Signup } from './User/SignUp';
