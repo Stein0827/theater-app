a830c2ef2d0a13dd9de8ef1ac03a6b4458af9643
Alan Castillo <alancastillo@Alans-MacBook-Air.local>
2022-12-15 00:07:27 -0500

    feat: client edit theater movies and theater components done

--- microservices/eventbus/src/controllers/publishController.ts
+++ microservices/eventbus/src/controllers/publishController.ts
@@ -4,12 +4,13 @@ import { eventBus } from '../models/eventBusModel';
 
 export const publishService = async (req: Request, res: Response) => {
     try {
+        console.log(eventBus)
         const data = req.body;
         const urlList = await eventBus.publish(data);
         console.log(`Success: Eventbus published data: ${JSON.stringify(data)} to subscribers ${JSON.stringify(urlList)}`);
         res.status(200).send(`Success: Eventbus published data: ${data} to subscribers ${urlList}`);
     } catch (err: any) {
         console.log("ERROR: EVENTBUS PUBLISH CONTROLLER ERR:", err)
-        res.status(400).send(`${err.name}: ${err.message}`);
+        res.status(400).send(err);
     }
 }
